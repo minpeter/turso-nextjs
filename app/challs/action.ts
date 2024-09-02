758a07b1cc18a47de20705c0c0f2cc386f811844
minpeter <minpeter@friendli.ai>
2024-09-02 18:08:59 +0900

    refactor: Remove unused code and files related to todo functionality

--- app/challs/action.ts
+++ app/challs/action.ts
@@ -5,15 +5,10 @@ import db from "@/libs/db/drizzle";
 export const getChalls = async () => {
   const data = await db.query.challenges.findMany();
 
-  // points: number;
-  // solves: number;
-
-  // 를 계산해서 data에 추가해줘야 함, 일단 랜덤으로
-  //TODO: 이 부분을 실제 데이터를 가지고 계산해야 함
-
   const result = data.map((d) => {
+    const { flag, dynamicImage, ...rest } = d;
     return {
-      ...d,
+      ...rest,
       points: Math.floor(Math.random() * 100),
       solves: Math.floor(Math.random() * 100),
     };
